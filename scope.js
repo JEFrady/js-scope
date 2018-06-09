696ed897e0e4d4b42a2313a4177d310d6b1cfdd7
Jamie Frady <jamiefrady@Jamies-MacBook-Pro.local>
2018-06-08 21:12:30 -1000

    All tests pass

--- scope.js
+++ scope.js
@@ -2,11 +2,13 @@
 var isCookieJarOpen = null;
 
 function closeLid() {
-  /* answer here */
+  isCookieJarOpen = false;
+  return isCookieJarOpen;
 }
 
 function openLid() {
-  /* answer here */
+  isCookieJarOpen = true;
+  return isCookieJarOpen;
 }
 
 
@@ -17,7 +19,7 @@ function outerFunction() {
 
   function innerFunction() {
     var world = 'World';
-    return /* answer here */;
+    return hello + " " + world;
   }
   return innerFunction();
 }
@@ -31,8 +33,8 @@ function addMatrixElements(matrix) {
 
   for(var i = 0; i < matrix.length; i++) {
     /* fix counter variables in the second loop */
-    for(var i = 0; i < matrix[i].length; i++) {
-      result = result + matrix[i][i];
+    for(var j = 0; j < matrix[i].length; j++) {
+      result = result + matrix[i][j];
     }
   }
   return result;
@@ -43,24 +45,21 @@ function addMatrixElements(matrix) {
 // Neo's information and not Morpheus'.  Fix it!
 function sendDataToClient() {
 
-  var userObject = {
-    handle: 'neo',
-    authenticated: false
-  }
+  var userObject = { handle: 'Neo', authenticated: false}
 
-  function authenticateUser(obj, username) {
-    var userObject = {
-      handle: 'morpheus',
-      authenticated: false
-    };
+    function authenticateUser(obj, username) {
+      var userObject = {handle: 'morpheus', authenticated: false};
 
-    if (userObject.handle === username) {
-      userObject.authenticated = true;
-      return userObject
-    } else {
-      return userObject
+            if (obj.handle === username) {
+              obj.authenticated = true;
+              return userObject
+            } 
+            
+            else {
+              return userObject
+            }
     }
-  }
-  authenticateUser(userObject, 'neo')
+
+  authenticateUser(userObject, 'Neo')
   return userObject
 }
